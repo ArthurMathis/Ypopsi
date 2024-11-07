9d784080b739e3c65a21631dfeaf8bffd6c7b755
Arthur Mathis <arthurmathis@MacBook-Pro-de-Arthur.local>
2024-11-07 10:25:27 +0100

    Patch de la navbarre : ajout des hver

--- layouts/pages/barres/navbarre.php
+++ layouts/pages/barres/navbarre.php
@@ -7,24 +7,24 @@
         </div>
     </article>
     <section class="action-section">
-        <a href="index.php" <?php if($currentPage == 'HOME') echo 'class="selected"' ?>>
+        <a href="index.php" class="LignesHover <?php if($currentPage == 'HOME') echo 'selected'?>">
             <img src="layouts/assets/img/logo/home.svg" alt="page d'accueil">
         </a>
-        <a href="index.php?applications=home">
+        <a href="index.php?applications=home" class="LignesHover <?php if($currentPage == 'APPLICATIONS') echo 'selected'?>">
             <img src="layouts/assets/img/logo/applications.svg" alt="liste des candidatures">
         </a>
-        <a href="#">
+        <a href="#" class="LignesHover <?php if($currentPage == 'NEEDS') echo 'selected'?>">
             <img src="layouts/assets/img/logo/needs.svg" alt="liste des besoins">
         </a>
-        <a href="#">
+        <a href="#" class="LignesHover <?php if($currentPage == 'STATS') echo 'selected'?>">
             <img src="layouts/assets/img/logo/statistics.svg" alt="statistiques">
         </a>
     </section>
     <section>
-        <a href="index.php?preferences=home">
+        <a href="index.php?preferences=home" class="LignesHover <?php if($currentPage == 'PREFERENCES') echo 'selected'?>">
             <img src="layouts/assets/img/logo/preferences.svg" alt="paramètres">
         </a>
-        <a href="index.php?login=deconnexion">
+        <a href="index.php?login=deconnexion" class="LignesHover">
             <img src="layouts/assets/img/logo/disconnect.svg" alt="déconnexion">
         </a>
     </section>
