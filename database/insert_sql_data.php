902ee61fdfd61c24787e1865519491526d1cfaa3
Arthur MATHIS <mathis.arthur68@gmail.com>
2025-02-19 15:39:59 +0100

    Ajout de la classe filePrinter

--- database/insert_sql_data.php
+++ database/insert_sql_data.php
@@ -3,6 +3,9 @@
 require_once("../vendor/autoload.php");
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 require_once('../define.php');
 
@@ -555,21 +558,160 @@ class sqlInterpreter {
 
 
 /**
+ * Class writing in a file
+ */
+class filePrinter {
+    /**
+     * Protected attribute containing the file
+     *
+     * @var Spreadsheet
+     */
+    protected Spreadsheet $sheet; 
+    /**
+     * Protected attribute containing the writer
+     *
+     * @var Xlsx
+     */
+    protected Xlsx $writer; 
+
+    /**
+     * Constructor class 
+     *
+     * @param string $path The path of the file
+     */
+    public function __construct(protected string $path) {
+        if(file_exists($this->getPath())) {
+            $this->sheet = IOFactory::load($this->getpath());                   // Opening the file
+        } else {
+            $this->sheet = new Spreadsheet();                                   // Creating new file
+        }
+
+        $this->writer = new Xlsx($this->getSheet());                            // Opening the writer
+
+
+        $title = $title = "Insertion des données du " . date('d/m/Y');
+
+        $this->addSheet($title);                                                // Creating the new sheet 
+    }
+
+    // * GET * //
+    /**
+     * Public method returning the path of the file
+     *
+     * @return string
+     */
+    public function getPath(): string { return $this->path; }
+    /**
+     * Public method returning the file
+     *
+     * @return Spreadsheet
+     */
+    public function getSheet(): Spreadsheet { return $this->sheet; }
+    /**
+     * Public method returning the writer
+     *
+     * @return Xlsx
+     */
+    public function getWriter(): Xlsx { return $this->writer; }
+
+    /**
+     * Public static method returning the based position of a new sheet
+     *
+     * @return int The index
+     */
+    public static function getBasedSheet(): int { return 0; }
+    /**
+     * Public static method returning the based column of the sheet
+     *
+     * @return string
+     */
+    public static function getBasedColumn(): string { return "A"; }
+
+    // * PRINT * //
+    /**
+     * Public method writing a row in the file
+     *
+     * @param int $row The index og the row
+     * @param array $data The data to write
+     * @return void
+     */
+    public function printRow(int $row, array &$data) {
+        $sheet = $this->getSheet()->getActiveSheet();
+
+        $column = filePrinter::getBasedColumn();
+
+        foreach($data as $obj) {
+            $sheet->setCellValue($column . $row, $obj);
+
+            $column++;
+        }
+    }
+
+    // * MANIPULATION * //
+    /**
+     * Public method registering the modifications
+     *
+     * @return void
+     */
+    public function save() { $this->getWriter()->save($this->getPath()); }
+
+    /**
+     * Protected method adding a new Sheet in the file
+     *
+     * @param string $sheetname The title of the sheet
+     * @return void
+     */
+    public function addSheet(string $sheetname) {
+        $worksheet = new Worksheet($this->getSheet(), filePrinter::getBasedSheet());                        // Creating the new sheet
+
+        $this->getSheet()->addSheet($worksheet);                                                            // Adding the new sheet
+
+        $this->getSheet()->getSheet(filePrinter::getBasedSheet())->setTitle($sheetname);                    // Setting the title 
+    }
+}
+
+
+/**
  * Class reading a file 
  */
 class fileReader {
+    /**
+     * Protected attribute containing the filePrinter writing the logs of registerings
+     *
+     * @var filePrinter
+     */
+    protected filePrinter $logsRegister;
+    /**
+     * Protected attribute containing the filePrinter writing the logs of errors
+     *
+     * @var filePrinter
+     */
+    protected filePrinter $errorsRegister;
+
     /**
      * Constructor class
      *
      * @param string $filePath The path of the file 
      * @param int $page The index of the page
+     * @param ?string $registerLogsPath The path of the logs of registerings
+     * @param ?string $errorsLogsPath The path of the errors logs
      */
     public function __construct(
         protected string $filePath,
-        protected int $page
+        protected int $page, 
+        ?string $registerLogsPath = null,
+        ?string $errorsLogsPath = null
     ) {
         if($page < 0) {
             die("Il est impossble de lire une feuille d'indice négatif");
+        }
+
+        if(! empty($registerLogsPath)) {
+            $this->logsRegister = new filePrinter($registerLogsPath);
+        }
+
+        if(! empty($errorsLogsPath)) {
+            $this->errorsRegister = new fileprinter($errorsLogsPath);
         }
     }
 
