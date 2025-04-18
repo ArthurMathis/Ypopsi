eb8606b4ab9cca658934689d8f6509e4275ceb6a
Arthur MATHIS <mathis.arthur68@gmail.com>
2025-04-18 15:23:02 +0200

    Refonte du système de tests unitaires

--- tests/config.php
+++ tests/config.php
@@ -5,5 +5,4 @@ require_once(__DIR__ . "/../vendor/autoload.php");
 use App\Core\ConfigManager;
 
 ConfigManager::errorSetting();
-ConfigManager::envLoad(__DIR__ . "/../.env");
-ConfigManager::envLoad(__DIR__ . "/../.tests.config.env");
+ConfigManager::envLoad(__DIR__ . "\.config.env");
